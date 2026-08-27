e71821bf9922bde5e1660cd2209b1fb4baed4309
Kyle Daryl B. Advincula <kbadvincula@sscgi.com>
2026-08-27 15:49:58 +0800

    chore: setup project and its dependencies
    feat: make initial migration

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
