0036119a8480f66cf8778643d077447cc86fc354
Kyle Daryl B. Advincula <kbadvincula@sscgi.com>
2026-08-27 16:30:52 +0800

    fix: Course.Credtis type from string to int

--- Migrations/20260827082238_Change Course.Credits' type from string to int.Designer.cs
+++ Migrations/20260827082238_Change Course.Credits' type from string to int.Designer.cs
@@ -4,6 +4,7 @@ using ContosoUniversity.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20260827082238_Change Course.Credits' type from string to int")]
+    partial class ChangeCourseCreditstypefromstringtoint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
