58c8f3c77978780654a9ed478cc3cbc4f0e88ddc
Kyle Daryl B. Advincula <kbadvincula@sscgi.com>
2026-08-28 11:43:11 +0800

    feat: implement polished crud operations across the controller and its views

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,10 +1,14 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int Id { get; set; }
         public string LastName { get; set; } = string.Empty;
         public string FirstMidName { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; } = [];
